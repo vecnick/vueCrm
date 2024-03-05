b782956191f0233e35a26a562e538a0f8377aa91
Nick <nikitaverhovod@gmail.com>
2024-03-06 01:01:56 +0300

    add shadcn

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,13 +1,11 @@
-<script lang="ts" setup {
-    
-}>
+<script lang="ts" setup>
 
 </script>
 
 <template>
-    <aside>
-        <NuxtLink to="/">
-            <NuxtImg src="/logo.svg" alt="" width=""/>
+    <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+        <NuxtLink to="/" class="mb-10 block">
+            <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto"/>
         </NuxtLink>
     </aside>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt'
+  ]
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
